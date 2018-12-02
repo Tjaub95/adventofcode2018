20394aeae9e91e24b18ef23f64ba17f67278b527
Tyler Jewell <tjaub95@gmail.com>
2018-12-01 18:12:09 -0600

    Got second part in Node

--- day1/day1.js
+++ day1/day1.js
@@ -1,10 +1,44 @@
 var fs = require('fs');
 
-fs.readFile('day1_input.txt', 'utf8', (err, contents) => {
-    if (err) throw err;
-    else {
-        let freq_arr = contents.split('\r\n');
-        const final_freq = freq_arr.reduce((total, freq) => parseInt(total) + parseInt(freq));
-        console.log(final_freq);
-    }
-});
+function get_input() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('day1_input.txt', 'utf8', (err, contents) => {
+            if (err) reject(err);
+            else resolve(contents.split('\r\n'));
+        });
+    });
+}
+
+function final_sum(input) {
+    return new Promise((resolve) => {
+        const final_freq = input.reduce((total, freq) => parseInt(total) + parseInt(freq));
+        resolve(final_freq);
+    });
+};
+
+function first_repeat_sum(input) {
+    return new Promise((resolve) => {
+        let sum = 0;
+        let known_values = new Set([sum])
+        safe_to_add = true;
+        while (safe_to_add) {
+            input.forEach(val => {
+                if (safe_to_add) sum += parseInt(val);
+                if (known_values.has(sum)) safe_to_add = false;
+                known_values.add(sum);
+            });
+        };
+        resolve(sum);
+    })
+};
+
+get_input()
+.then((data) => {
+    final_sum(data)
+    .then((data) => console.log("First solution is " + data))
+    .catch((err) => console.log(err));
+
+    first_repeat_sum(data)
+    .then((data) => console.log("Second solution is " + data))
+    .catch((err) => console.log(err));
+})

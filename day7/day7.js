833bf40da4aee5ceb25f0f52666096976e85b51a
tjaub95 <tjaub95@gmail.com>
2018-12-07 15:04:38 -0600

    Finish day 7

--- day7/day7.js
+++ day7/day7.js
@@ -11,34 +11,63 @@ function get_input() {
 
 function determine_step_order(input) {
     let steps = {};
-    for (const line of input) {
-        let [/*Step*/, stepCurr, /*must*/, /*be*/, /*finished*/, /*before*/, /*step*/, stepItPrecedes] = line.split(" ");
-        if (steps[stepCurr]) {
-            steps[stepCurr].push(stepItPrecedes);
-        }
-        else {
-            steps[stepCurr] = [stepItPrecedes];
-        };
-    }
-    
-    console.log(steps)
+    input.forEach(line => {
+        line = line.split(' ');
+        const stepCurr = line[1];
+        const stepItPrecedes = line[7];
+        steps[stepCurr] = steps[stepCurr] || {node:stepCurr, leaves:{}};
+        steps[stepItPrecedes] = steps[stepItPrecedes] || {node:stepItPrecedes, leaves:{}};
+        steps[stepItPrecedes].leaves[stepCurr] = true;
+    });
+    let stepsOrder = '';
+    while(true) {
+        const step = Object.values(steps).filter(node => !Object.keys(node.leaves).length).sort((a,b)=> a.node < b.node ? -1 : 1);
+        if(!step[0]) break;
+        const node = step[0].node;
+        stepsOrder += node;
+        delete steps[node];
+        Object.values(steps).forEach(o => delete o.leaves[node]);
+    };
 
-    stepOrder = [];
-    let stepsKeys = Object.keys(steps);
-    let counter = 0;
-    for (step in steps) {
-        if (steps[stepsKeys[counter]].includes(step)) {
-            // This step comes after this key
-            console.log(step)
-        } else {
-            // step precedes this key
-            counter++;
-        }
-    }
+    return stepsOrder;
+}
+
+function build_steps_graph(input) {
+    let steps = {};
+    input.forEach(line => {
+        line = line.split(' ');
+        const stepCurr = line[1];
+        const stepItPrecedes = line[7];
+        steps[stepCurr] = steps[stepCurr] || {node:stepCurr, leaves:{}, duration: stepCurr.charCodeAt(0) - 4};
+        steps[stepItPrecedes] = steps[stepItPrecedes] || {node:stepItPrecedes, leaves:{}, duration: stepItPrecedes.charCodeAt(0) - 4};
+        steps[stepItPrecedes].leaves[stepCurr] = true;
+    });
     return steps;
 }
 
+function calculate_time_to_complete(steps, result, seconds) {
+    let queue = Object.values(steps).filter(node => Object.keys(node.leaves).length === 0);
+    
+    if (queue.length === 0) return seconds;
+    seconds++;
+
+    for (let i = 0; i < 5; i++) {
+        let worker = ({...queue[i]}).node;
+
+        if (steps[worker] && --steps[worker].duration < 1) {
+            Object.values(steps).forEach(node => delete node.leaves[worker]);
+            delete steps[worker];
+        }
+    }
+
+    return calculate_time_to_complete(steps, result, seconds);
+}
+
+
+
 get_input()
 .then((data) => {
+    let steps = build_steps_graph(data);
     console.log("First solution is " + determine_step_order(data));
+    console.log("Second solution is " + calculate_time_to_complete(steps, '', 0));
 });
